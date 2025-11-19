0b6b19de78f296777bc63583075a04eb20455a28
olexandr0209 <olexandrkrychylskiy@gmail.com>
2025-11-19 12:57:03 +0100

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,10 @@ import os
 APP_ENV = os.getenv("APP_ENV", "dev")
 
 if APP_ENV == "prod":
+    # Продакшен: основний бот + основна база
     BOT_TOKEN = os.getenv("BOT_TOKEN")
     DATABASE_URL = os.getenv("DATABASE_URL")
 else:
+    # Dev: тестовий бот + тестова база (або та сама, якщо ти так зробив)
     BOT_TOKEN = os.getenv("BOT_TOKEN_DEV")
     DATABASE_URL = os.getenv("DATABASE_URL_DEV") or os.getenv("DATABASE_URL")
