02c8152479268b8fc4468c29e4477c6389e9b05a
olexandr0209 <olexandrkrychylskiy@gmail.com>
2025-11-19 12:26:33 +0100

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,7 @@ APP_ENV = os.getenv("APP_ENV", "dev")
 
 if APP_ENV == "prod":
     BOT_TOKEN = os.getenv("BOT_TOKEN")
+    DATABASE_URL = os.getenv("DATABASE_URL")
 else:
     BOT_TOKEN = os.getenv("BOT_TOKEN_DEV")
+    DATABASE_URL = os.getenv("DATABASE_URL_DEV") or os.getenv("DATABASE_URL")
