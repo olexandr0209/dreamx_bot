1810b71b6e3acddc426fd13afa1e11878abe84b2
olexandr0209 <olexandrkrychylskiy@gmail.com>
2025-11-19 13:22:27 +0100

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,12 @@ import os
 APP_ENV = os.getenv("APP_ENV", "dev")
 
 if APP_ENV == "prod":
-    # Продакшен: основний бот + основна база
+    # Продакшн: головний бот
     BOT_TOKEN = os.getenv("BOT_TOKEN")
     DATABASE_URL = os.getenv("DATABASE_URL")
+    WEBAPP_URL = "https://dreamx-webapp.onrender.com"
 else:
-    # Dev: тестовий бот + тестова база (або та сама, якщо ти так зробив)
+    # Dev: тестовий бот
     BOT_TOKEN = os.getenv("BOT_TOKEN_DEV")
-    DATABASE_URL = os.getenv("DATABASE_URL_DEV") or os.getenv("DATABASE_URL")
+    DATABASE_URL = os.getenv("DATABASE_URL_DEV")
+    WEBAPP_URL = "https://dreamx-webapp-dev.onrender.com"
